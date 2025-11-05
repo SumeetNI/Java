8a2c5f78ec45ba33d63feff5dbb754c4a7682445
SumeetNL <sumeetnilange70@gmail.com>
2025-11-05 19:40:28 +0530

    java basics

--- variables.java
+++ variables.java
@@ -1,5 +1,111 @@
-public class variables {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+// public class variables {
+// public static void main(String[] args) {
+// System.out.println("Hello, World!");
+// }
+// }
+
+// public class variables {
+// public static void main(String[] args) {
+// System.out.println("****");
+// System.out.println("***");
+// System.out.println("**");
+// System.out.println("*");
+// }
+// }
+
+/* public class variables {
+
+public static void main(String[] arg) {
+int a = 2;
+int b = 5;
+System.out.println(a);
+System.out.println(b);
 }
+}
+*/
+
+// public class variables {
+
+// public static void main(String[] args) {
+// int a = 4;
+// float b = 4.11f;
+// short c = 2;
+// long d = 8;
+// double e = 8;
+// byte f = 8;
+// boolean g = true;
+// char h = 2;
+
+// System.out.println(a);
+// System.out.println(b);
+// System.out.println(c);
+// System.out.println(d);
+// System.out.println(f);
+// System.out.println(g);
+// System.out.println(h);
+// }
+// }
+
+// public class variables {
+
+// public static void main(String[] args) {
+// int a = 2;
+// int b = 5;
+// System.out.println(a+b);
+// }
+// }
+
+// import java.util.*;
+// public class variables {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner (System.in);
+//     int a = sc.nextInt();
+//     float b = sc.nextFloat();
+//     double c = sc.nextDouble();
+//     long d = sc.nextLong();
+//     Short e = sc.nextShort();
+//     System.out.println("a is :"+a);
+//     System.out.println("b is :"+b);
+//     System.out.println("c is :"+c);
+//     System.out.println("d is :"+d);
+//     System.out.println("e is :"+e);
+// }
+// }
+
+// import java.util.*;
+// public class variables {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         System.out.println(a+b);
+//     }
+// }
+
+// import java.util.*;
+// public class variables {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner (System.in);
+//     float r = sc.nextFloat();
+
+//     float area = 3.14f * r *r;
+//     System.out.println(area);
+// }
+
+// }
+
+// type conversion
+// public class variables {
+
+//     public static void main(String[] args) {
+//         int a = 10;
+//         long b = a;
+//         // type casting
+//         long c = 20;
+//         int d = (int) c;
+//         // float c= 10.12f;
+//         // int d = (int) c;
+//         System.out.println(d);
+//     }
+// }
