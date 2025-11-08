bd2418b86a4fbe6b43586e05311eea67efaccb7b
SumeetNL <sumeetnilange70@gmail.com>
2025-11-08 08:15:24 +0530

    Operaters

--- Operater.java
+++ Operater.java
@@ -1,3 +1,54 @@
+//post increment
+/*
 public class Operater {
-    
+    public static void main(String[] args) {
+        int a =1;
+        int b = a++;
+        System.out.println("a is:"+a);
+        System.out.println("b is:"+b);
+    }
 }
+*/
+
+//pre increment
+/*
+public class Operater {
+    public static void main(String[] args) {
+        int a =1;
+        int b = ++a;
+        System.out.println("a is:"+a);
+        System.out.println("b is:"+b);
+    }
+}
+*/
+
+
+/* 
+public class Operater {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+
+        }
+}
+*/
+
+
+/* 
+public class Operater {
+
+    public static void main(String[] args) {
+        System.out.println((3>2)&&(4>3));
+        System.out.println((3>2)||(3>3));
+        System.out.println((3>2)!=(4>3));
+    }
+}
+*/
+
